06491049be030c3a5d9deb978ab262ab7241fbc6
ricardosousa1989 <sousa.ricardo10@gmail.com>
2013-03-05 15:30:35 +0000

    adding test file, for testing unit tests and code coverage options of Visual Studio 2012.

---
+++ STrackerServer/STrackerServer.Tests/Tests/HomeController_Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using STrackerServer.Controllers;
+
+namespace STrackerServer.Tests
+{
+    [TestClass]
+    public class HomeController_Test
+    {
+
+        private HomeController _homeController = new HomeController();
+
+        [TestMethod]
+        public void TestIndex()
+        {   
+            Assert.AreEqual(_homeController.Index(), "Hello World");
+        }
+    }
+}
