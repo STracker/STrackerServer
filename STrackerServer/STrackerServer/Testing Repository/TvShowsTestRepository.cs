9e31171d91c78cad55b21353cf734e348b611a01
ricardosousa1989 <sousa.ricardo10@gmail.com>
2013-03-23 20:33:31 +0000

    create TvShowController and action to show info on user story 1. Create memory repository for tvshows for testing proposes.

---
+++ STrackerServer/STrackerServer/Testing Repository/TvShowsTestRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using STrackerServer.Models;
+
+namespace STrackerServer.Testing_Repository
+{
+    public class TvShowsTestRepository
+    {  
+        private readonly IDictionary<int, TvShow> _dictionary = new Dictionary<int, TvShow>();
+
+        public TvShowsTestRepository()
+        {
+            var tvshow = new TvShow
+                {
+                    ImdbId = 1,
+                    Title = "The Walking Dead",
+                    Description = "zombies apocalypse!!!",
+                    Runtime = 45,
+                    Rating = 4
+                };
+
+            _dictionary.Add(1, tvshow);
+        }
+
+        public void Add(TvShow tvShow)
+        {
+            _dictionary.Add(tvShow.ImdbId, tvShow);
+        }
+
+        public TvShow Get(int id)
+        {
+            TvShow tvshow;
+            _dictionary.TryGetValue(id, out tvshow);
+            return tvshow;
+        }
+    }
+}
